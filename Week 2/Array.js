4c3f5f30c017e9e833aaaf4697d8a64836a37982
GillesCovens <92446554+GillesCovens@users.noreply.github.com>
2022-02-16 15:14:37 +0100

    g

---
+++ Week 2/Array.js
@@ -0,0 +1,9 @@
+const setup = () => {
+}
+window.addEventListener("load", setup);
+
+let famillieLeden = ["Michiel", "Milan", "Yassin"]
+for(let i = 0; i< famillieLeden.length; i++)
+{
+    console.log(i);
+}
