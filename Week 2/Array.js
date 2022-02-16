f947317961e2dca954da98f6d9cb7eb78f1bd1e3
GillesCovens <92446554+GillesCovens@users.noreply.github.com>
2022-02-16 15:59:36 +0100

    oefening1

--- Week 2/Array.js
+++ Week 2/Array.js
@@ -1,9 +1,31 @@
+let famillieLeden = ["Michiel", "Milan", "Yassin", 'bertje', 'gertjan']
+let aantal = 0;
+for(let i = 0; i< famillieLeden.length; i++)
+{
+    aantal++;
+}
+console.log(aantal);
+
+console.log(famillieLeden[0]);
+console.log(famillieLeden[2]);
+console.log(famillieLeden[4]);
+
+
+const voegNaamToe = () =>
+{
+    famillieLeden.unshift(prompt('pls enter a name'));
+    console.log(famillieLeden);
+}
+
+voegNaamToe();
+
+console.log(famillieLeden.join(" "));
+
+
+
+
 const setup = () => {
 }
 window.addEventListener("load", setup);
 
-let famillieLeden = ["Michiel", "Milan", "Yassin"]
-for(let i = 0; i< famillieLeden.length; i++)
-{
-    console.log(i);
-}
+
