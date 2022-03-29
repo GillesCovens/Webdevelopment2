8759947d0c176c9472fa690d37482d0efd8a25ac
GillesCovens <92446554+GillesCovens@users.noreply.github.com>
2022-03-29 19:43:09 +0200

    hh

---
+++ Week 5/Trigrams/index.js
@@ -0,0 +1,10 @@
+const setup = () => {
+    let word = 'onoorbaar';
+
+    for(let i = 0; i+2 < word.length; i++)
+    {
+        console.log(word[i] + word[i+1] + word[i+2]);
+    }
+}
+window.addEventListener("load", setup);
+window.addEventListener("load", setup);
